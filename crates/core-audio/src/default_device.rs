2016d5723a00f53893ae2e0014af23f0c1a2aeca
poluce <poluce@users.noreply.github.com>
2026-08-26 13:29:03 +0800

    修复IPolicyConfig字符串封送导致默认设备切换不生效

--- crates/core-audio/src/default_device.rs
+++ crates/core-audio/src/default_device.rs
@@ -22,18 +22,18 @@ public class PolicyConfigClient { }
 
 [ComImport, Guid(""f8679f50-850a-41cf-9c72-430f290290c8""), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
 public interface IPolicyConfig {
-    [PreserveSig] int GetMixFormat(string pszDeviceName, IntPtr ppFormat);
-    [PreserveSig] int GetDeviceFormat(string pszDeviceName, bool bDefault, IntPtr ppFormat);
-    [PreserveSig] int ResetDeviceFormat(string pszDeviceName);
-    [PreserveSig] int SetDeviceFormat(string pszDeviceName, IntPtr pEndpointFormat, IntPtr MixFormat);
-    [PreserveSig] int GetProcessingPeriod(string pszDeviceName, bool bDefault, IntPtr pmftDefaultPeriod, IntPtr pmftMinimumPeriod);
-    [PreserveSig] int SetProcessingPeriod(string pszDeviceName, IntPtr pmftPeriod);
-    [PreserveSig] int GetShareMode(string pszDeviceName, IntPtr pMode);
-    [PreserveSig] int SetShareMode(string pszDeviceName, IntPtr mode);
-    [PreserveSig] int GetPropertyValue(string pszDeviceName, bool bFxStore, IntPtr key, IntPtr pv);
-    [PreserveSig] int SetPropertyValue(string pszDeviceName, bool bFxStore, IntPtr key, IntPtr pv);
-    [PreserveSig] int SetDefaultEndpoint(string wszDeviceId, int eRole);
-    [PreserveSig] int SetEndpointVisibility(string pszDeviceName, bool bVisible);
+    [PreserveSig] int GetMixFormat([MarshalAs(UnmanagedType.LPWStr)] string pszDeviceName, IntPtr ppFormat);
+    [PreserveSig] int GetDeviceFormat([MarshalAs(UnmanagedType.LPWStr)] string pszDeviceName, bool bDefault, IntPtr ppFormat);
+    [PreserveSig] int ResetDeviceFormat([MarshalAs(UnmanagedType.LPWStr)] string pszDeviceName);
+    [PreserveSig] int SetDeviceFormat([MarshalAs(UnmanagedType.LPWStr)] string pszDeviceName, IntPtr pEndpointFormat, IntPtr MixFormat);
+    [PreserveSig] int GetProcessingPeriod([MarshalAs(UnmanagedType.LPWStr)] string pszDeviceName, bool bDefault, IntPtr pmftDefaultPeriod, IntPtr pmftMinimumPeriod);
+    [PreserveSig] int SetProcessingPeriod([MarshalAs(UnmanagedType.LPWStr)] string pszDeviceName, IntPtr pmftPeriod);
+    [PreserveSig] int GetShareMode([MarshalAs(UnmanagedType.LPWStr)] string pszDeviceName, IntPtr pMode);
+    [PreserveSig] int SetShareMode([MarshalAs(UnmanagedType.LPWStr)] string pszDeviceName, IntPtr mode);
+    [PreserveSig] int GetPropertyValue([MarshalAs(UnmanagedType.LPWStr)] string pszDeviceName, bool bFxStore, IntPtr key, IntPtr pv);
+    [PreserveSig] int SetPropertyValue([MarshalAs(UnmanagedType.LPWStr)] string pszDeviceName, bool bFxStore, IntPtr key, IntPtr pv);
+    [PreserveSig] int SetDefaultEndpoint([MarshalAs(UnmanagedType.LPWStr)] string wszDeviceId, int eRole);
+    [PreserveSig] int SetEndpointVisibility([MarshalAs(UnmanagedType.LPWStr)] string pszDeviceName, bool bVisible);
 }
 "@
 $client = New-Object PolicyConfigClient
